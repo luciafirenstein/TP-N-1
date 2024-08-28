f98caf6708eb6de51f53b08fc013de87914740b8
luciafirenstein <lfirenstein@gmail.com>
2024-08-28 02:02:36 -0300

    CambioFinal

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function mostrarHora() {
+    const reloj = document.getElementById('reloj');
+    const hora = new Date().toLocaleTimeString();
+    reloj.textContent = hora;
+}
+setInterval(mostrarHora, 1000);
 
+document.addEventListener('DOMContentLoaded', function() {
+    const formulario = document.getElementById('comentario-form');
+    const mensajeConfirmacion = document.getElementById('mensaje-confirmacion');
+
+    formulario.addEventListener('submit', function(event) {
+        event.preventDefault(); 
+
+        const comentario = document.getElementById('comentario').value;
+        mensajeConfirmacion.textContent = '¡Gracias por tu comentario!';
+
+        formulario.reset();
+
+        setTimeout(() => {
+            mensajeConfirmacion.textContent = '';
+        }, 1000000);
+    });
+});
--- pag1.js
+++ pag1.js
@@ -1,3 +1,9 @@
+function mostrarHora() {
+    const reloj = document.getElementById('reloj');
+    const hora = new Date().toLocaleTimeString();
+    reloj.textContent = hora;
+}
+setInterval(mostrarHora, 1000);
 
 
 
